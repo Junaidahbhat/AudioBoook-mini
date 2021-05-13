e956bc1e3ff7a1dce04d15f5907cd1d548be4e13
Junaid <59776318+Junaidahbhat@users.noreply.github.com>
2021-05-13 21:41:26 +0530

    Update audiobook.py

--- audiobook.py
+++ audiobook.py
@@ -1,14 +1,16 @@
 import pyttsx3     #step 1
 import PyPDF2
-book = open('a1.pdf','rb') #step 3 get pdf file
-pdfReader = PyPDF2.PdfFileReader(book) #for reading the book
+book = open('a1.pdf','rb')                          #step 3 get pdf file
+pdfReader = PyPDF2.PdfFileReader(book)              #for reading the book
 pages = pdfReader.numPages
 print(pages)
-speaker = pyttsx3.init()              #initilizing it step 2
-page = pdfReader.getPage(28)           #step 4 for  to read page no.29
-text = page.extractText()                 #for extracting text from pdf
-speaker.say(text)
-speaker.runAndWait()                    #speaker.say('hey look i can talk') step 1
+speaker = pyttsx3.init()                  #initilizing it step 2
+for num in range(13,pages):                 # will start from page number 14 on the pdf
+    page = pdfReader.getPage(13)           #step 4 for  to read from page no 14.
+    text = page.extractText()                 #for extracting text from pdf
+    speaker.say(text)
+    speaker.runAndWait()                    #speaker.say('hey look i can talk') 
+
 
 # use pip install pyttsx3 and pip install PyPDF2 to install these two files.
 
